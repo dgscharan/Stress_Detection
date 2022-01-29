9e21fecba7a1b6debf3cdaee820893897b7b31b9
sri charan <dgscharan@gmail.com>
2022-01-28 20:05:24 -0500

    Update README.md

---
+++ EMA_Survey/scratch.py
@@ -0,0 +1,6 @@
+'Gen_Disrespect= ' + ema_df.gen_disrespect[i],
+            'COVID_concern= ' + ema_df.COVID_concern[i], 'Discomfort= ' + ema_df.Discomfort[i],
+            'Lack_support= ' + ema_df.Lack_support[i],   'Team_value= ' + ema_df.Team_value[i],
+            'Demands= ' + ema_df.Demands[i], 'Death= ' + ema_df.Death[i], 
+            'Other_work-stress= ' + ema_df['Other_work-stress'][i],  'Other_non-work-stress= ' + ema_df['Other_non-work-stress'][i] ))
+    
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Stress_Detection
 this code extract high stressful moments from data collected from E4 Empatica sensors. this region of interest is determined by taking a threshold which is one standard deviation above the mean of each data. 
 
+CMD - Execution:
+
+python Stress_detection_script.py 
 Arguments:
   * File Path
   * File Name with Extension
